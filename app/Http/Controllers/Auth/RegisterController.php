9287ea7fafd8d73e56c20106c43086f0e075fdd3
Bryan Kottama <bryan@kyodo-i.com>
2020-05-03 16:18:33 +0700

    add mail notification after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\LarashopMail;
 use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -64,10 +67,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        if ($user) {
+            Mail::to($data['email'])->send(new LarashopMail);
+            return $user;
+        }
+        return redirect('/')->with('Gagal mendaftar', 'Sepertinya ada kendala di database kami');
     }
 }
---
+++ resources/views/emails.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Selamat Bergabung
+
+Selamat anda telah terdaftar sebagai anggota Larashop.com
+
+@component('mail::button', ['url' => 'https://larashop.com'])
+Klik disini
+@endcomponent
+
+Terimakasih,<br>
+Larashop
+@endcomponent
